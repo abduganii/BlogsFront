deda7142d8ff65ee73f2a3a834afce5fb9de9b12
Abdugani Abduqaxxorov <83403180+abduganii@users.noreply.github.com>
2022-12-07 18:43:07 +0500

    Initial commit

---
+++ src/pages/AddPost/index.jsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import SimpleMDE from 'react-simplemde-editor';
+
+import 'easymde/dist/easymde.min.css';
+import styles from './AddPost.module.scss';
+import { useSelector } from 'react-redux';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
+
+
+export const AddPost = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const isAuth = useSelector(selectIsAuth);
+  const [isLoading, setIsloading] = React.useState('');
+  const [text, setText] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const [tags, setTags] = React.useState('');
+  const [imageURL, setImageUrl] = React.useState('');
+  const inputRef = React.useRef(null)
+
+  const isEditing = Boolean(id)
+
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData()
+      const file = event.target.files[0]
+      formData.append('image', file)
+      const { data } = await axios.post('/upload', formData)
+
+      setImageUrl(data.url);
+    } catch (error) {
+      console.warn(error);
+      alert('File upload error')
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl('')
+  };
+
+  const onChange = React.useCallback((value) => {
+    setText(value);
+  }, []);
+
+  const onSubmit = async () => {
+    try {
+      setIsloading(true)
+
+      const fields = {
+        title,
+        tags,
+        text,
+        imageURL,
+      }
+
+      const { data } = isEditing ?
+        await axios.put(`/posts/${id}`, fields) :
+        await axios.post('/posts', fields)
+
+      const _id = isEditing ? id : data.data._id
+
+      navigate(`/posts/${_id}`)
+    } catch (error) {
+      console.warn(error);
+      alert('Error creating articles')
+    }
+  };
+
+  // React.useEffect(() => {
+  //   if (id) {
+  //     axios
+  //       .get(`/posts/${id}`)
+  //       .then(({ data }) => {
+  //         setTitle(data.title)
+  //         setText(data.text)
+  //         setImageUrl(data.imageURL)
+  //         setTags(data.tags.join(','))
+
+  //       })
+  //   }
+  // })
+
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: '400px',
+      autofocus: true,
+      placeholder: 'Введите текст...',
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    [],
+  );
+
+  if (!window.localStorage.getItem("token") && !isAuth) {
+    return <Navigate to='/' />
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button onClick={() => inputRef.current.click()} variant="outlined" size="large">
+        Download preview
+      </Button>
+      <input ref={inputRef} type="file" onChange={handleChangeFile} hidden />
+      {imageURL && (
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Delete
+          </Button>
+
+          <img className={styles.image} src={`http://localhost:5555${imageURL}`} alt="Uploaded" />
+        </>
+      )}
+
+      <br />
+      <br />
+      <TextField
+        classes={{ root: styles.title }}
+        variant="standard"
+        placeholder="Заголовок статьи..."
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        fullWidth
+      />
+      <TextField
+        value={tags}
+        onChange={e => setTags(e.target.value)}
+        classes={{ root: styles.tags }}
+        variant="standard"
+        placeholder="Тэги"
+        fullWidth
+      />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
+      <div className={styles.buttons}>
+        <Button onClick={onSubmit} size="large" variant="contained">
+          {isEditing ? "Save" : 'Publish'}
+        </Button>
+        <a href="/">
+          <Button size="large">Cancel</Button>
+        </a>
+      </div>
+    </Paper>
+  );
+};
